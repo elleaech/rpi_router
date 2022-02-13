534d206d219a710770fc42f6ff82595faa0c1fd8
elleaech <lohan.chuan123@gmail.com>
2022-02-13 16:54:05 -0300

    Added initial configuration logic

--- rpi_router.py
+++ rpi_router.py
@@ -1,9 +1,50 @@
 from router_data import *
 from router_cmd import *
 
+CLIENT = RTFile()
+GATEWAY = RTFile()
+
+CONF_DIRECTORY = RTPath()
+
+DHCP_CLIENT = RTDhcpClient()
+DHCP_SERVER = RTDhcpServer()
+
+FIREWALL = RTFireWall()
+
+
+def set_network() -> bool:
+    CONF_DIRECTORY.append(CLIENT)
+    CONF_DIRECTORY.append(GATEWAY)
+
+    CONF_DIRECTORY.go()
+
+    CLIENT.set_addr()
+    GATEWAY.set_addr()
+
+    CONF_DIRECTORY.goback()
+
+
+def set_dhcp_server() -> bool:
+    DHCP_SERVER.install()
+    DHCP_SERVER.enable()
+    DHCP_SERVER.configure()
+
+
+def set_firewall() -> bool:
+    FIREWALL.allow_dhcp_traffic()
+    FIREWALL.enable_package_forwarding()
+
 
 def main() -> int:
-    pass
+    DHCP_CLIENT.disable()
+
+    success: bool = set_network()
+
+    if success:
+        success: bool = set_dhcp_server()
+
+        if success:
+            set_firewall()
 
 
 if __name__ == "__main__":
